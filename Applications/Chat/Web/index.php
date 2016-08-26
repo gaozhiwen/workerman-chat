9c27ce12567914a5f9e0743f3f22389546e96dbd
wen <1165737969@qq.com>
2016-08-26 20:57:42 +0800

    update

--- Applications/Chat/Web/index.php
+++ Applications/Chat/Web/index.php
@@ -4,12 +4,12 @@
   <script type="text/javascript">
   //WebSocket = null;
   </script>
-  <link href="/css/bootstrap.min.css" rel="stylesheet">
-  <link href="/css/style.css" rel="stylesheet">
+  <link href="./css/bootstrap.min.css" rel="stylesheet">
+  <link href="./css/style.css" rel="stylesheet">
   <!-- Include these three JS files: -->
-  <script type="text/javascript" src="/js/swfobject.js"></script>
-  <script type="text/javascript" src="/js/web_socket.js"></script>
-  <script type="text/javascript" src="/js/jquery.min.js"></script>
+  <script type="text/javascript" src="./js/swfobject.js"></script>
+  <script type="text/javascript" src="./js/web_socket.js"></script>
+  <script type="text/javascript" src="./js/jquery.min.js"></script>
 
   <script type="text/javascript">
     if (typeof console == "undefined") {    this.console = { log: function (msg) {  } };}
@@ -151,7 +151,7 @@
                </form>
                <div>
                &nbsp;&nbsp;&nbsp;&nbsp;<b>房间列表:</b>（当前在&nbsp;房间<?php echo isset($_GET['room_id'])&&intval($_GET['room_id'])>0 ? intval($_GET['room_id']):1; ?>）<br>
-               &nbsp;&nbsp;&nbsp;&nbsp;<a href="/?room_id=1">房间1</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="/?room_id=2">房间2</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="/?room_id=3">房间3</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="/?room_id=4">房间4</a>
+               &nbsp;&nbsp;&nbsp;&nbsp;<a href="./?room_id=1">房间1</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="./?room_id=2">房间2</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="./?room_id=3">房间3</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="./?room_id=4">房间4</a>
                <br><br>
                </div>
                <p class="cp">PHP多进程+Websocket(HTML5/Flash)+PHP Socket实时推送技术&nbsp;&nbsp;&nbsp;&nbsp;Powered by <a href="http://www.workerman.net/workerman-chat" target="_blank">workerman-chat</a></p>
@@ -160,7 +160,7 @@
 	           <div class="thumbnail">
                    <div class="caption" id="userlist"></div>
                </div>
-               <a href="http://workerman.net:8383" target="_blank"><img style="width:252px;margin-left:5px;" src="/img/workerman-todpole.png"></a>
+               <a href="http://workerman.net:8383" target="_blank"><img style="width:252px;margin-left:5px;" src="./img/workerman-todpole.png"></a>
 	        </div>
 	    </div>
     </div>
