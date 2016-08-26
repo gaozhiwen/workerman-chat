b7c7ec29529e7897d6ceb8b617e4d8abb2909236
wen <1165737969@qq.com>
2016-08-26 21:18:59 +0800

    update

--- Applications/Chat/Web/index.php
+++ Applications/Chat/Web/index.php
@@ -13,7 +13,7 @@
 
   <script type="text/javascript">
     if (typeof console == "undefined") {    this.console = { log: function (msg) {  } };}
-    WEB_SOCKET_SWF_LOCATION = "/swf/WebSocketMain.swf";
+    WEB_SOCKET_SWF_LOCATION = "./swf/WebSocketMain.swf";
     WEB_SOCKET_DEBUG = true;
     var ws, name, client_list={};
 
